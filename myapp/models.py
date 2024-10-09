faf5358bf2423e6f59e1c16e366a2171c2afd02f
jeanpaulufps <ivanjeanpauldi@ufps.edu.co>
2024-10-09 16:31:48 -0500

    added models

--- myapp/models.py
+++ myapp/models.py
@@ -18,15 +18,67 @@ class Usuario(models.Model):
         abstract = True  # Indica que es una clase abstracta
 
 
+class Pensum(models.Model):
+    pass
+
+
+class Departamento(models.Model):
+    nombre = models.CharField(max_length=100)
+
+
+class Carrera(models.Model):
+    nombre = models.CharField(max_length=100)
+    codigo = models.CharField(max_length=20)
+    pensum = models.ForeignKey(
+        Pensum, on_delete=models.CASCADE, related_name="carreras"
+    )
+    semestres = models.IntegerField()
+    modalidad = models.IntegerField()
+    tipoCarrera = models.IntegerField()
+    departamento = models.ForeignKey(
+        Departamento, on_delete=models.CASCADE, related_name="carreras"
+    )
+
+
 class Estudiante(Usuario):
-    # materiasMatriculadas =                            #RELACION
-    # carrera =
+    materiasMatriculadas = models.ManyToManyField(
+        "Materia", related_name="estudiantes_matriculados", blank=True
+    )
+    carrera = models.ManyToManyField(Carrera)
     creditosAprobados = models.IntegerField()
     estadoMatricula = models.IntegerField()
 
 
+class Materia(models.Model):
+    nombre = models.CharField(max_length=100)
+    codigo = models.CharField(max_length=20)
+    creditos = models.IntegerField()
+    intensidadHoraria = models.IntegerField()
+    requisito = models.ManyToManyField(
+        "self", symmetrical=False, related_name="materias_requisito", blank=True
+    )
+
+
 class Profesor(Usuario):
-    materias = models.CharField(max_length=1)  # RELACION
+    pass
+
+
+class Horario(models.Model):
+    horaInicio = models.TimeField()
+    horaFin = models.TimeField()
+    dia = models.IntegerField()
+    aula = models.CharField(max_length=200)
+
+
+class Grupo(models.Model):
+    codigo = models.CharField(max_length=20)
+    materia = models.ForeignKey(
+        Materia, on_delete=models.CASCADE, related_name="grupos"
+    )
+    profesores = models.ManyToManyField(Profesor, related_name="grupos")
+    horario = models.ForeignKey(
+        Horario, on_delete=models.CASCADE, related_name="grupos"
+    )
 
 
 class Nota(models.Model):
@@ -34,8 +86,15 @@ class Nota(models.Model):
     segunda = models.FloatField()
     tercera = models.FloatField()
     cuarta = models.FloatField()
-    # estudiante =                                           #RELACION
-    # materia =                                              #RELACION
+    estudiante = models.ForeignKey(
+        Estudiante, on_delete=models.CASCADE, related_name="notas"
+    )
+    materia = models.ForeignKey(Materia, on_delete=models.CASCADE, related_name="notas")
 
 
-# class Departamento(models.Model):
+class Semestre(models.Model):
+    numeroSemestre = models.IntegerField()
+    pensum = models.ForeignKey(
+        Pensum, on_delete=models.CASCADE, related_name="semestres"
+    )
+    materias = models.ManyToManyField(Materia, related_name="semestres")
