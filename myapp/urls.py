75f7977b60e8e7034c6eff9cb0fef236bf031f27
Jean Paul <ivanjeanpauldi@ufps.edu.co>
2024-10-10 16:21:02 -0500

    student get endpoints

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index),
-    path("about", views.about),
+    path("api/estudiantes", views.get_estudiantes),
+    path('api/estudiantes/<int:estudiante_id>/', views.get_estudiante_por_id),
 ]
