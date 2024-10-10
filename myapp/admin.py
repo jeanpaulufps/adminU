ed578149f541dcd62853ff722f725c5c65c3927b
jeanpaulufps <ivanjeanpauldi@ufps.edu.co>
2024-10-10 16:09:13 -0500

    models added to admin interface

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from myapp.models import (
+    Pensum,
+    Departamento,
+    Profesor,
+    Horario,
+    Carrera,
+    Estudiante,
+    Materia,
+    Grupo,
+    Nota,
+    Semestre,
+)
+
+# from myapp import models
 
 # Register your models here.
+
+admin.site.register(Pensum)
+admin.site.register(Departamento)
+admin.site.register(Profesor)
+admin.site.register(Horario)
+admin.site.register(Carrera)
+admin.site.register(Estudiante)
+admin.site.register(Materia)
+admin.site.register(Grupo)
+admin.site.register(Nota)
+admin.site.register(Semestre)
