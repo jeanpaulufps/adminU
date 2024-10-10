c467b52159a4fd65e39a330c5171a86930641879
Jean Paul <ivanjeanpauldi@ufps.edu.co>
2024-10-10 16:19:56 -0500

    project root urls modified

--- adminu/urls.py
+++ adminu/urls.py
@@ -19,6 +19,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("admin/", admin.site.urls), 
-    path("home/", include("myapp.urls"))
-    ]
+    path("admin/", admin.site.urls),
+    path("", include("myapp.urls")),
+]
