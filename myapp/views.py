f3f2a66a9613afac29fefb2bb61982fb9621edc4
Jean Paul <ivanjeanpauldi@ufps.edu.co>
2024-10-10 16:21:27 -0500

    student getter methods

--- myapp/views.py
+++ myapp/views.py
@@ -1,11 +1,20 @@
 # from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import JsonResponse
+from myapp.models import Estudiante
+from django.forms.models import model_to_dict
+from .models import Estudiante
 
 # Create your views here.
 
 
-def index(request):
-    return HttpResponse('<h1>Index</h1>')
+def get_estudiantes(request):
+    estudiantes = Estudiante.objects.all().values()
+    return JsonResponse(list(estudiantes), safe=False)
 
-def about(request):
-    return HttpResponse('<h1>About</h1>')
+
+def get_estudiante_por_id(request, estudiante_id):
+    try:
+        estudiante = Estudiante.objects.get(id=estudiante_id)
+        return JsonResponse(model_to_dict(estudiante))
+    except Estudiante.DoesNotExist:
+        return JsonResponse({'error': 'Estudiante no encontrado'}, status=404)
