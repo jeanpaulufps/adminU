d6d5d2ec3a4ed553a603ae60cf472cffaf802d3e
Jean Paul <ivanjeanpauldi@ufps.edu.co>
2024-11-16 12:54:43 -0500

    solve some bugs

--- api/models.py
+++ api/models.py
@@ -42,6 +42,7 @@ class Usuario(AbstractBaseUser):
     fechaNacimiento = models.DateField()
     direccion = models.CharField(max_length=255)
     telefono = models.CharField(max_length=20)
+    celular = models.CharField(max_length=20)
     correoElectronico = models.CharField(max_length=50, unique=True)
     correoInstitucional = models.CharField(max_length=50, unique=True)
     fechaIngreso = models.DateField()
--- api/serializers.py
+++ api/serializers.py
@@ -34,12 +34,26 @@ class ProfesorSerializer(serializers.ModelSerializer):
 
 
 class HorarioSerializer(serializers.ModelSerializer):
-    dia_nombre = serializers.CharField(source="get_dia_display", read_only=True)
+    materia = serializers.CharField(source="materia.nombre") 
+    aula = serializers.CharField(source="aula.codigo", default=None)
+
+    dia_nombre = serializers.SerializerMethodField()
 
     class Meta:
         model = models.Horario
-        fields = '__all__'
+        fields = ["materia",  "horaInicio", "horaFin", "dia", "dia_nombre", "aula"]
 
+    def get_dia_nombre(self, obj):
+        dias = {
+            1: "Lunes",
+            2: "Martes",
+            3: "Miércoles",
+            4: "Jueves",
+            5: "Viernes",
+            6: "Sábado",
+            7: "Domingo",
+        }
+        return dias.get(obj.dia, "Desconocido")
 
 class CarreraSerializer(serializers.ModelSerializer):
     class Meta:
